6f8af2465da9b6d6d8e02c8a929fc654589bdec5
abh1sh3k47 <abh1sh3k47@gmail.com>
2015-07-20 10:49:54 +0530

    adding Order type ( Skeleton )

---
+++ StockMarketGeneralCodes/src/com/algoTrader/types/IOrder.java
@@ -0,0 +1,9 @@
+package com.algoTrader.types;
+
+// Just a Marker interface for now.
+// Can contain getter setter methods with generic args, broker implementations would set required broker specific data accordingly. 
+public interface IOrder 
+{
+	
+
+}
