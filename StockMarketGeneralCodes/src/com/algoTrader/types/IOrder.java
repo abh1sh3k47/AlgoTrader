b54eff2cf283a9b12d3095c02e215d65f3fe92c5
abh1sh3k47 <abh1sh3k47@gmail.com>
2015-07-20 10:52:36 +0530

    Revert "adding Order type ( Skeleton )"
    This reverts commit 6f8af2465da9b6d6d8e02c8a929fc654589bdec5.

--- StockMarketGeneralCodes/src/com/algoTrader/types/IOrder.java
+++
@@ -1,9 +0,0 @@
-package com.algoTrader.types;
-
-// Just a Marker interface for now.
-// Can contain getter setter methods with generic args, broker implementations would set required broker specific data accordingly. 
-public interface IOrder 
-{
-	
-
-}
